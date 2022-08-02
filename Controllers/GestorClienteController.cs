fe02a80c854f18a70a0832df79d9ad63f01d8746
Deyvifcruz <98196701+Deyvifcruz@users.noreply.github.com>
2022-08-02 14:13:38 -0400

    Add files via upload

---
+++ Controllers/GestorClienteController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GestorCliente.Models;
+using GestorCliente.Models.ViewModels;
+namespace GestorCliente.Controllers
+{
+    public class GestorClienteController : Controller
+    {
+        // GET: GestorCliente
+        public ActionResult Index()
+        {
+            List<GestorClienteVM> lst;
+            using (DBGestorClientesEntities db = new DBGestorClientesEntities()) 
+            {
+                lst = (from d in db.Cliente
+                       select new GestorClienteVM
+                       {
+                           Id = d.Id,
+                           Nombre = d.Nombre,
+                           Direccion1 = d.Direccion1,
+                           Direccion2 = d.Direccion2,
+                           Direccion3 = d.Direccion3
+                       }).ToList();
+            }
+
+                return View(lst);
+        }
+    }
+}
---
+++ Models/ViewModels/GestorClienteVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestorCliente.Models.ViewModels
+{
+    public class GestorClienteVM
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion1 { get; set; }
+        public string Direccion2 { get; set; }
+        public string Direccion3 { get; set; }
+    }
+}
